ec2a8fac194e2687d183cd8d2abd05eba99bbd9b
strayd0g <vndrewboss@gmail.com>
2022-05-04 00:36:19 +0800

    New Update: [Version: 1.0.11]

--- main.php
+++ main.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://tugasvirtualsolutions.com/
  * Author: Tugas Virtual Solutions
  * Author URI: https://tugasvirtualsolutions.com/
- * Version: 1.0.10
+ * Version: 1.0.11
  * Description: A plug-in that can add stores, generate QR and QR page router.
  * Text-Domain: tugasvirtualsolution.com
  * 
