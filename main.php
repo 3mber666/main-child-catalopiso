c825f3b45b390fdc568f844c704a0809decb179a
strayd0g <vndrewboss@gmail.com>
2022-05-04 23:25:20 +0800

    Version Update: 1.1.5

--- main.php
+++ main.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://tugasvirtualsolutions.com/
  * Author: Tugas Virtual Solutions
  * Author URI: https://tugasvirtualsolutions.com/
- * Version: 1.1.4
+ * Version: 1.1.5
  * Description: A plug-in that can add stores, generate QR and QR page router.
  * Text-Domain: tugasvirtualsolution.com
  * 
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 === WP Store [Catalopiso Child Stores] ===
-Requires at least: 1.1.4
+Requires at least: 1.1.5
 Tested up to: 7.3.5
 Requires PHP: 5.2
 Stable tag: main
