4b642b4aa3ee211a1c6cf9113a423b153918dee6
strayd0g <vndrewboss@gmail.com>
2022-05-05 23:21:53 +0800

    Fixed: Removing Mailer

--- main.php
+++ main.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://tugasvirtualsolutions.com/
  * Author: Tugas Virtual Solutions
  * Author URI: https://tugasvirtualsolutions.com/
- * Version: 1.1.8
+ * Version: 1.1.9
  * Description: A plug-in that can add stores, generate QR and QR page router.
  * Text-Domain: tugasvirtualsolution.com
  * 
@@ -60,6 +60,6 @@ $myUpdateChecker->setAuthentication('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');
 require_once WPRK_PATH . 'classes/class-create-admin-menu.php';
 require_once WPRK_PATH . 'classes/class-create-settings-routes.php';
 require_once WPRK_PATH . 'loaders/receiver.php';
-require_once WPRK_PATH . 'loaders/mailer.php';
+// require_once WPRK_PATH . 'loaders/mailer.php';
 require_once WPRK_PATH . 'loaders/install.php';
 require_once WPRK_PATH . 'lib/helpers.php';
