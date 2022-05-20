55b26f2dab4b15dae2fa2da6adf2d991a79d484b
strayd0g <vndrewboss@gmail.com>
2022-05-20 15:01:37 +0800

    Updated: Queries, Crons Job

--- lib/helpers.php
+++ lib/helpers.php
@@ -216,7 +216,7 @@ function getUsersYesterday() {
     $user_data_table.store_code,
     $store_data_table.store_url
     FROM $user_data_table
-    INNER JOIN wp_store_codes
+    INNER JOIN $store_data_table
     ON $user_data_table.store_code=$store_data_table.store_code WHERE DATE(timestamp) = CURRENT_DATE()-1;";
     
 
@@ -338,18 +338,19 @@ function my_cron_schedules($schedules){
 function schedule_my_cron(){
     // Schedules the event if it's NOT already scheduled.
     if ( ! wp_next_scheduled ( 'my_5min_event' ) ) {
-        wp_schedule_event( time(), '5min', 'my_5min_event' );
+        wp_schedule_event( time(), '30min', 'my_5min_event' );
     }
 }
 
 function fivemin_schedule_hook() {
+
     global $wpdb;
     
     $user_data_table = $wpdb->prefix . "users_store_data";
     $store_data_table = $wpdb->prefix . "store_codes";
 
     
-    $get_user_x = $wpdb->get_results ("SELECT  $user_data_table.id, 
+    $get_user_x = $wpdb->get_results ("SELECT $user_data_table.id, 
     $user_data_table.timestamp, 
     $user_data_table.name, 
     $user_data_table.email, 
@@ -359,20 +360,13 @@ function fivemin_schedule_hook() {
     $user_data_table.store_code,
     $store_data_table.store_url
     FROM $user_data_table
-    INNER JOIN wp_store_codes
+    INNER JOIN $store_data_table
     ON $user_data_table.store_code=$store_data_table.store_code WHERE DATE(timestamp) = CURRENT_DATE()-1");
 
     foreach ($get_user_x as $geturldata ) {
         $body = "Your Project Board $geturldata->store_url/?password_protected_pwd=$geturldata->store_code&redirect_to=/project-boards/?key=$geturldata->key";
         wp_mail($geturldata->email, 'Here\'s the link of your project board for this day', $body);
     }
-
-    $admin_email = get_option( 'admin_email' );
-    $site_url = get_site_url();
-
-    $body = "Your Project Board $site_url/yesterday?code=yew";
-    wp_mail($admin_email, 'Here\'s the link of your project board for this day', $body);
-
 }
 
 ?>
--- main.php
+++ main.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://tugasvirtualsolutions.com/
  * Author: Tugas Virtual Solutions
  * Author URI: https://tugasvirtualsolutions.com/
- * Version: 1.3.10
+ * Version: 1.3.11
  * Description: A plug-in that can add stores, generate QR and QR page router.
  * Text-Domain: tugasvirtualsolution.com
  * 
@@ -40,9 +40,9 @@ function add_link_x( $links ) {
 	return $links;
 }
 
-// add_action( 'init', 'schedule_my_cron' );
-// add_action( 'my_5min_event', 'fivemin_schedule_hook' );
-// add_filter( 'cron_schedules','my_cron_schedules' );
+add_action( 'init', 'schedule_my_cron' );
+add_action( 'my_5min_event', 'fivemin_schedule_hook' );
+add_filter( 'cron_schedules','my_cron_schedules' );
 
 
 require 'plugin-update-checker/plugin-update-checker.php';
